64b2ae5c7f7046726c31b944c9a53be19cf68839
Timotius Nugroho Chandra <timotius.n.chandra@gdpventure.com>
2014-09-26 22:43:30 +0700

    1. Added backstretch 2. Added javascript for navigating

--- scripts/scripts.0a635f8a.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("lelyvitoweddingApp",[]),angular.module("lelyvitoweddingApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
