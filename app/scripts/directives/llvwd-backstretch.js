ca84c76e673d60f2c3e860e88a83d6f101cb1916
Timotius Nugroho Chandra <timotius.n.chandra@gdpventure.com>
2014-09-27 21:57:14 +0700

    Added functionality for go-top button

--- app/scripts/directives/llvwd-backstretch.js
+++ app/scripts/directives/llvwd-backstretch.js
@@ -12,6 +12,15 @@ angular.module('lelyvitoweddingApp')
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
         $.backstretch("images/save-lg.jpg");
+        
+        var goToTop = $('#go-top');
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 100) {
+            goToTop.fadeIn();
+          } else {
+            goToTop.fadeOut();
+          }
+        });
       }
     };
   });
